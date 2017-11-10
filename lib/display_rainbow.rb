b1c934020ad1e88994cfbc48138672ea3c7e17d2
Yadiel Cabrera <ycabr035@fiu.edu>
2017-11-10 02:28:27 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,5 +6,5 @@ def display_rainbow (colors = ["red","orange","yellow","green","blue","indigo","
   puts "G:#{colors[3]}"
   puts "B:#{colors[4]}"
   puts "I:#{colors[5]}"
-  puts "V:{#{colors[6]}}"
+  puts "V:#{colors[6]}"
 end
